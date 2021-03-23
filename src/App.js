90073d73a376ec88b7447b49c4454c07a0146c62
GiuseppeFitzsimmons <fitzsimmonsgiuseppe@gmail.com>
2021-03-23 17:01:46 +0100

    basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { React } from 'react';
+import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Header from './widgets/header'
 
 function App() {
   return (
     <div>
+      <Header />
       A
     </div>
   );
