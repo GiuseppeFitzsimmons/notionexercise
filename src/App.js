6e8db495c07d4f1953309497760c76142ca7f67e
GiuseppeFitzsimmons <fitzsimmonsgiuseppe@gmail.com>
2021-03-23 18:35:23 +0100

    header migration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import Header from './widgets/header'
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
@@ -35,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
   salutation: {
     flexGrow: 1,
     color: 'black',
-    fontStyle:'bold'
+    fontStyle: 'bold'
   }
 }));
 
@@ -43,32 +43,18 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
-  const [location, setLocation] = React.useState('')
+  const [location, setLocation] = React.useState('home')
   console.log("location", location)
   return (
     <>
       <Header location={location} />
       <Router>
         <div className={classes.root}>
-      <AppBar position="absolute" color='transparent' style={{position:'absolute',borderRadius:'4px', left:'5%', top:'5%',width:'90%'}}>
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            from router config
-          </Typography>
-          <Typography variant="h7" className={classes.salutation} style={{fontStyle:'bold'}}>
-            from router config
-          </Typography>
-          
-          <ExitToApp color="primary" />
-        </Toolbar>
-      </AppBar>
-      <Fab size="small" color="primary" aria-label="add" className={classes.floatingButton}>
-        <AddIcon />
-      </Fab>
           <ul>
-            <button onClick={() => { setLocation('home') }}>
-              <Link to="/" state={'home'}>Home Page</Link>
-            </button>
+            <Button onClick={() => { setLocation('home') }}>
+              Home Page
+              <Redirect to="/" />
+            </Button>
           </ul>
           <Switch>
             <Route exact path="/">
@@ -76,6 +62,9 @@ function App() {
             </Route>
             <Route path="/interventions">
               <Interventions />
+              <Button onClick={() => { setLocation('interventions') }}>
+                <Link to="/interventions"> INTERVENTIONS </Link>
+              </Button>
             </Route>
           </Switch>
         </div>
@@ -83,19 +72,17 @@ function App() {
     </>
   );
   function Home(props) {
-    console.log('home props', props)
     return (
       <div>
         <h2>Home</h2>
-        <button onClick={() => { setLocation('interventions') }}>
-          <Link to="/interventions">Interventions</Link>
-        </button>
+        <Button onClick={() => { setLocation('interventions') }}>
+          <Redirect to="/interventions" />
+        </Button>
       </div>
     );
   }
 
   function Interventions(props) {
-    console.log('interventions props', props)
     return (
       <div />
     );
--- src/widgets/header.js
+++ src/widgets/header.js
@@ -1,10 +1,56 @@
 import { React } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { ExitToApp } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    floatingButton: {
+        position: 'absolute',
+        bottom: theme.spacing(4),
+        right: theme.spacing(4)
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 3,
+        color: 'blue'
+    },
+    salutation: {
+        flexGrow: 1,
+        color: 'black',
+        fontStyle: 'bold'
+    }
+}));
+
 
 export default function Header(props) {
     console.log('header props', props)
+    const classes = useStyles();
     return (
         <div>
-            {props?.location}
+            <AppBar position="absolute" color='transparent' style={{ position: 'absolute', borderRadius: '4px', left: '5%', top: '5%', width: '90%' }}>
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        {props?.location}
+                    </Typography>
+                    <Typography variant="h7" className={classes.salutation} style={{ fontStyle: 'bold' }}>
+                        from router config
+          </Typography>
+
+                    <ExitToApp color="primary" />
+                </Toolbar>
+            </AppBar>
+            <Fab size="small" color="primary" aria-label="add" className={classes.floatingButton}>
+                <AddIcon />
+            </Fab>
         </div>
     )
 }
